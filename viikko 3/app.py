da9bfde65f5b9d8b8263590764662a84c2126ede
Joni Koskela <joni.koskela2@gmail.com>
2025-11-15 23:04:06 +0200

    Create app.py for Meteorite Dashboard homepage

---
+++ viikko 3/app.py
@@ -0,0 +1,10 @@
+# app.py (pääsivu)
+
+import streamlit as st
+
+st.title("Tervetuloa Meteorite Dashboardiin")
+
+st.write("Klikkaa alla olevaa linkkiä nähdäksesi meteorittidata-analyysin:")
+
+
+st.markdown("[Siirry Data Analysis -sivulle](/data_analysis)")
